cb3d536a1c857f6d9bb1162e1e8898f5f3538f85
Annika2357 <annika.treutle@gmail.com>
2025-05-26 13:08:02 +0200

    zylinder und disk gefixed, höhe und radius war vertauscht

--- src/environment.py
+++ src/environment.py
@@ -10,9 +10,9 @@ class Environment:
         self.use_gui = use_gui
         self.agent_start_pos    = [0.0, 0.0, 0.2]
         self.agent_start_ori    = [0, 0, 0, 1]
-        self.cylinder_start_pos = [2.0, 2.0, 0]
+        self.cylinder_start_pos = [2.0, 2.0, 0.5]
         self.cylinder_start_ori = [0, 0, 0, 1]
-        self.disk_start_pos     = [-1.5, 0, 10]
+        self.disk_start_pos     = [-1.5, 0, 0]
         self.disk_start_ori     = [0, 0, 0, 1]
         self.pyramid_start_pos  = [1.4, 0.2, 0.0]
         self.pyramid_start_ori  = [0, 0, 0, 1]
@@ -67,16 +67,16 @@ class Environment:
 
         self.cylinder_id = p.createMultiBody(
             baseMass=1,
-            baseCollisionShapeIndex=p.createCollisionShape(p.GEOM_CYLINDER, radius=0.7, height=0.1),
-            baseVisualShapeIndex=p.createVisualShape(p.GEOM_CYLINDER, radius=0.7, length=0.1,
+            baseCollisionShapeIndex=p.createCollisionShape(p.GEOM_CYLINDER, radius=0.2, height=1.0),
+            baseVisualShapeIndex=p.createVisualShape(p.GEOM_CYLINDER, radius=0.2, length=1.0,
                                                      rgbaColor=[1, 0, 0, 1]),
             basePosition=self.cylinder_start_pos
         )
 
         self.disk_id = p.createMultiBody(
             baseMass=1,
-            baseCollisionShapeIndex=p.createCollisionShape(p.GEOM_CYLINDER, radius=0.2, height=1.0),
-            baseVisualShapeIndex=p.createVisualShape(p.GEOM_CYLINDER, radius=0.2, length=1.0,
+            baseCollisionShapeIndex=p.createCollisionShape(p.GEOM_CYLINDER, radius=0.7, height=0.1),
+            baseVisualShapeIndex=p.createVisualShape(p.GEOM_CYLINDER, radius=0.7, length=0.1,
                                                      rgbaColor=[0, 0, 1, 1]),
             basePosition=self.disk_start_pos
         )
